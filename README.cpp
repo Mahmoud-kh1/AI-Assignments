b8650d187b86225f74818debbbe592f9712864b0
Mahmoud khaled <162627248+Mahmoud-kh1@users.noreply.github.com>
2024-03-20 05:10:25 +0200

    Update README.md

--- README.cpp
+++ README.cpp
@@ -1,19 +1,3 @@
 
-This project to solve a basic Game called 8-puzzle which it has 8 numbers and one cell as empty cell ... the goal is to make it ordered in correct order and make 
-the empty cell is the last one 
-ex :  1 2 3
-      4 5 *
-      7 8 6
-      -----------------------------------------------------------------------------------------------------------------------------------------------------------
-      the valid move is to swap the empty cell and one of its neighbors up and down and  left and right if its in the grid bounds
-      in this example we can just swap it with 6 (down move) 
-      it will be 
-      1 2 3
-      4 5 6
-      7 8 *
-      which is our Goal 
-      -----------------------------------------------------------------------------------------------------------------------------------------------------------
-      there is many algorthim to do it .... I will solve it using BFS, DFS, Greedy , A* 
-      
-
+    HarvardCs50 Ai projects
       
