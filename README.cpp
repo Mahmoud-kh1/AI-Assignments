98a9be3ca3cc55a459df94992deee875c7f5083f
Mahmoud khaled <162627248+Mahmoud-kh1@users.noreply.github.com>
2024-03-20 05:05:51 +0200

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -1,5 +1,5 @@
          Assignment for College
-This project to solve a basic Game calle 8-puzzle which it has 8 numbers and one cell as empty cell ... the goal is to make it ordered in correct order and make 
+This project to solve a basic Game called 8-puzzle which it has 8 numbers and one cell as empty cell ... the goal is to make it ordered in correct order and make 
 the empty cell is the last one 
 ex :  1 2 3
       4 5 *
